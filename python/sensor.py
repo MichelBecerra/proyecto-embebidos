9ebf9afb34cc90a3f5c4248e4baf11f4df0a32aa
Michel Becerra <michelandreabc@gmail.com>
2018-03-27 00:36:09 -0600

    changing typos and adding a main

--- python/sensor.py
+++ python/sensor.py
@@ -20,19 +20,29 @@ class Sensor():
 
     def init_sensor(self):
         '''Create serial'''
-        self.serial = serial.Serial(port_names(), BAUD, TIMEOUT)
+        self.serial = serial.Serial(port_names(self), BAUD, TIMEOUT)
 
-def read_data():
-    '''Read data from sensors
-       data[0] --> sensor 1
-       data[1] --> sensor 2
-       data[2] --> sensor 3'''
-    data = ''
-    while(self.serial.inWaiting() > 0):
+    def read_data(self):
+        '''Read data from sensors
+        data[0] --> sensor 1
+        data[1] --> sensor 2
+        data[2] --> sensor 3'''
+        data = ''
+        while(self.serial.inWaiting() > 0):
+            data = self.srl.readline().decode('utf-8')
         data = self.srl.readline().decode('utf-8')
-    data = self.srl.readline().decode('utf-8')
-    print(data)
-    data_1 = data.split(' ')[0]
-    data_2 = data.split(' ')[1]
-    data_3 = data.split(' ')[2]
-    return data_1, data_2, data_3
+        print(data)
+        data_1 = data.split(' ')[0]
+        data_2 = data.split(' ')[1]
+        data_3 = data.split(' ')[2]
+        return data_1, data_2, data_3
+
+def main():
+    srl = Sensor()
+    srl.init_sensor()
+    while True:
+        data_1, data_2, data_3 = srl.read_data()
+    pass
+
+if __name__ == '__main__':
+    main()
