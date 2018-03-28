63bc996cea86219f4c422a51a28afed69e198b55
Michel Becerra <michelandreabc@gmail.com>
2018-03-27 21:11:03 -0600

    merge the files

---
+++ python/conection_demeter.py
@@ -0,0 +1,53 @@
+from firebase import firebase
+
+# select/get
+def selectContenedor(contenedor):
+    fb = firebase.FirebaseApplication('https://demeter-siade.firebaseio.com/', None)
+    address = '/Sensores/' + contenedor
+    result = fb.get(address, None)
+    print(result)
+    return result
+
+# update/post
+def updateCantidad(contenedor, cantidad):
+    fb = firebase.FirebaseApplication('https://demeter-siade.firebaseio.com/', None)
+    address = '/Sensores/' + contenedor
+    result = fb.put(address, 'cantidad', cantidad)
+    print(result)
+    return result
+
+contenedor = raw_input("Contenedor: ")
+valor = raw_input("Valor: ")
+updateCantidad(contenedor, valor)
+
+
+
+#from firebase import firebase
+#from firebase_admin import db
+#import threading
+#import time
+
+#class Conection(threading.Thread):
+#    def __init__(self, cb):
+#        threading.Thread.__init__(self)
+#        self.callback = cb
+#        self.fire = firebase.FirebaseApplication('https://demeter-siade.firebaseio.com/', None)
+
+#ref = db.reference('Sensores/contenedorCuatro')
+#users_ref = ref.child('')
+
+#import pyrebase
+#config = {
+#  "apiKey": "AIzaSyDheOhXfu5CKMWiRMhrmjVWYfGKA7C0ves",
+#  "authDomain": "demeter.firebaseapp.com",
+#  "databaseURL": "demeter-siade.firebaseapp.com",
+#  "storageBucket": "demeter-siade.appspot.com"
+  #"serviceAccount": "path/to/serviceAccountCredentials.json"
+#}
+
+#firebase = pyrebase.initialize_app(config)
+
+#auth = firebase.auth()
+#authenticate a user
+#user = auth.sign_in_with_email_and_password("alan061997@gmail.com", "Alan4Ever")
+#demeter-siade.child("Sensores").child("contenedorDos").update({"cantidad": "2"}, user['idToken'])
---
+++ python/sensor_demeter.py
@@ -0,0 +1,69 @@
+import time
+import serial
+import serial.tools.list_ports as listPorts
+import conection_demeter
+
+BAUD = 9600
+TIMEOUT = 1
+
+product_list = {
+    "lata de champiñones" : [0, 9.5],
+    "botella de agua" : [0, 11]
+}
+
+class Sensor():
+    def __init__(self, *args, **kwargs):
+        '''Init sensor'''
+        self.serial = None
+
+    def port_names(self):
+        '''Return portname'''
+        pnames = []
+        for port in listPorts.comports():
+            if "ttyAMA" in port[0]:
+                continue
+            else:
+                pnames.append(port[0])
+        return pnames[0]
+
+    def init_sensor(self):
+        '''Create serial'''
+        self.serial = serial.Serial(self.port_names(), BAUD, timeout= TIMEOUT)
+
+    def read_data(self):
+        '''Read data from sensors
+        data[0] --> sensor 1
+        data[1] --> sensor 2
+        data[2] --> sensor 3'''
+        data = ''
+        while(self.serial.inWaiting() > 0):
+            data = self.serial.readline().decode('utf-8').strip()
+        data = self.serial.readline().decode('utf-8').strip()
+        data_1 = data.split(' ')[0]
+        data_2 = data.split(' ')[1]
+        data_3 = data.split(' ')[2]
+        return data_1, data_2, data_3
+
+def calculate_products():
+    for product in product_list.keys():
+        product
+
+def main():
+    #srl = Sensor()
+    #srl.init_sensor()
+    #print ("Leyendo datos ...")
+    #time.sleep(3)
+    while True:
+        time.sleep(2)
+        #data_1, data_2, data_3 = srl.read_data()
+        data_1 = 1
+        data_2 = 2
+        data_3 = 3
+        print ("Distance 1: {}\nDistance 2: {}\nDistance 3: {}\n\n".format(data_1, data_2, data_3))
+        updateCantidad("contenedorUno", data_1)
+        updateCantidad("contenedorDos", data_2)
+        updateCantidad("contenedorTres", data_3)
+
+
+if __name__ == '__main__':
+    main()
