b5f5959ca24d786ee7adffa580d77039e92a15ac
Michel Becerra <michelandreabc@gmail.com>
2018-03-27 19:40:17 -0600

    def changing

--- python/conection.py
+++ python/conection.py
@@ -3,15 +3,15 @@ from firebase import firebase
 # select/get
 def selectContenedor(contenedor):
     fb = firebase.FirebaseApplication('https://demeter-siade.firebaseio.com/', None)
-    contenedor2 = '/Sensores/' + contenedor
-    result = fb.get(contenedor2, None)
+    address = '/Sensores/' + contenedor
+    result = fb.get(address, None)
     print(result)
     return result
 
 # update/post
 def updateCantidad(contenedor, cantidad):
     fb = firebase.FirebaseApplication('https://demeter-siade.firebaseio.com/', None)
-    contenedor2 = '/Sensores/' + contenedor
-    result = fb.put(contenedor2, 'cantidad', cantidad)
+    address = '/Sensores/' + contenedor
+    result = fb.put(address, 'cantidad', cantidad)
     print(result)
     return result
