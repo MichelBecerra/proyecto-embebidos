6416f218930039efcd0c762247b96422b1df01cf
Michel Becerra <michelandreabc@gmail.com>
2018-03-27 19:33:37 -0600

    modify str

--- python/conection.py
+++ python/conection.py
@@ -1,7 +1,7 @@
 from firebase import firebase
 
 # select/get
-def selectContenedor(self, contenedor):
+def selectContenedor(contenedor):
     firebase = firebase.FirebaseApplication('https://demeter-siade.firebaseio.com/', None)
     contenedor2 = '/Sensores/' + contenedor
     result = firebase.get(contenedor2, None)
@@ -9,7 +9,7 @@ def selectContenedor(self, contenedor):
     return result
 
 # update/post
-def updateCantidad(self, contenedor, cantidad):
+def updateCantidad(contenedor, cantidad):
     firebase = firebase.FirebaseApplication('https://demeter-siade.firebaseio.com/', None)
     contenedor2 = '/Sensores/' + contenedor
     result = firebase.put(contenedor2, 'cantidad', cantidad)
