3cc7a7bb09f9530d2e856e3db6de5ebb39d00e67
Michel Becerra <michelandreabc@gmail.com>
2018-03-27 19:35:47 -0600

    fb label

--- python/conection.py
+++ python/conection.py
@@ -2,16 +2,16 @@ from firebase import firebase
 
 # select/get
 def selectContenedor(contenedor):
-    firebase = firebase.FirebaseApplication('https://demeter-siade.firebaseio.com/', None)
+    fb = firebase.FirebaseApplication('https://demeter-siade.firebaseio.com/', None)
     contenedor2 = '/Sensores/' + contenedor
-    result = firebase.get(contenedor2, None)
+    result = fb.get(contenedor2, None)
     print(result)
     return result
 
 # update/post
 def updateCantidad(contenedor, cantidad):
-    firebase = firebase.FirebaseApplication('https://demeter-siade.firebaseio.com/', None)
+    fb = firebase.FirebaseApplication('https://demeter-siade.firebaseio.com/', None)
     contenedor2 = '/Sensores/' + contenedor
-    result = firebase.put(contenedor2, 'cantidad', cantidad)
+    result = fb.put(contenedor2, 'cantidad', cantidad)
     print(result)
     return result
